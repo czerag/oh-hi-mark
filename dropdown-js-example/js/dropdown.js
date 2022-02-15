550c84a18a03e854b920a7ad0d1644480f1a5c8c
Cezary Chmura <67397536+czerag@users.noreply.github.com>
2022-02-15 11:28:43 +0100

    Added window onload function

--- dropdown-js-example/js/dropdown.js
+++ dropdown-js-example/js/dropdown.js
@@ -1,14 +1,23 @@
-var content = document.getElementById("content");
-var button = document.getElementById("show-more");
+function dropdown(){
+    var content = document.getElementById("content");
+    var button = document.getElementById("show-more");
+    var message = document.getElementById("message");
+    
+    button.onclick = function(){
+        if(content.classList.contains("active")){
+            //revert
+            content.classList.toggle("active");
+            message.classList.toggle("active");
+            button.innerHTML = "Show more";
+        } else {
+            //make fun of user
+            content.classList.toggle("active");
+            message.classList.toggle("active");
+            button.innerHTML = "wait...";
+        }
+    };
+}
 
-button.onclick = function(){
-    if(content.classList.contains("active")){
-        //revert
-        content.classList.toggle("active");
-        button.innerHTML = "Show more";
-    } else {
-        //make fun of user
-        content.classList.toggle("active");
-        button.innerHTML = "Skip";
-    }
+window.onload = function(){
+    dropdown();
 };
